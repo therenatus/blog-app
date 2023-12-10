9b357084183405752931edc933d29469a3411805
therenatus <therinatus@gmail.com>
2023-12-10 17:22:51 +0600

    fix recovery password router(new password input middleware with recovery code validation)

--- src/controller/validator/set-newPassword.validator.ts
+++ src/controller/validator/set-newPassword.validator.ts
@@ -5,14 +5,14 @@ const repository = new UserRepository();
 
 export const SetNewPasswordValidator = [
   body("newPassword").trim().isLength({ min: 6, max: 20 }),
-  // body("recoveryCode")
-  //   .trim()
-  //   .isString()
-  //   .custom(async (recoveryCode) => {
-  //     const user = await repository.getOneByCode(recoveryCode);
-  //     if (!user) {
-  //       throw new Error("user not found");
-  //     }
-  //     return true;
-  //   }),
+  body("recoveryCode")
+    .trim()
+    .isString()
+    .custom(async (recoveryCode) => {
+      const user = await repository.getOneByCode(recoveryCode);
+      if (!user) {
+        throw new Error("user not found");
+      }
+      return true;
+    }),
 ];
