2dec3f4842f0306ffa758869cafda321d898a7e4
therenatus <therinatus@gmail.com>
2023-12-29 18:09:56 +0600

    create like business layer

---
+++ src/buisness/like.business.ts
@@ -0,0 +1,55 @@
+import { PostRepository } from "../repositories/post.repository";
+import { PostLikeRepository } from "../repositories/post-like.repository";
+import { injectable } from "inversify";
+import { LikeStatus, LikeType } from "../types/like.type";
+import { PostLikeModel } from "../model/postLike.model";
+import { UserRepository } from "../repositories/user.repository";
+import { HydratedDocument } from "mongoose";
+
+@injectable()
+export class LikeBusinessLayer {
+  constructor(
+    protected repository: PostRepository,
+    protected likeRepository: PostLikeRepository,
+    protected userRepository: UserRepository,
+  ) {}
+  async prepareCommentForLike(
+    postId: string,
+    userId: string,
+    status: LikeStatus,
+  ) {
+    let post = await this.likeRepository.findLike(postId, userId);
+    let user = await this.userRepository.findOneById(userId);
+    if (!user) {
+      return null;
+    }
+    if (!post) {
+      post = this.newLike(postId, user, status);
+    }
+    post = await this.updateLike(post, status);
+    return this.likeRepository.save(post);
+  }
+
+  private newLike(
+    postId: string,
+    user: any,
+    likeStatus: LikeStatus,
+  ): HydratedDocument<LikeType> {
+    console.log("user", user);
+    return new PostLikeModel({
+      postId,
+      userId: user.accountData.id,
+      likeStatus,
+      createdAt: new Date(),
+      userInfo: user._id, //d
+    });
+  }
+
+  private async updateLike(
+    like: HydratedDocument<LikeType>,
+    likeStatus: LikeStatus,
+  ) {
+    like.likeStatus === likeStatus;
+    return like;
+  }
+}
