800b7c5968e4ebd757b18e5b25fda857e95763aa
therenatus <therinatus@gmail.com>
2024-01-10 15:24:31 +0600

    create validate model function

--- src/blogs/blog.service.ts
+++ src/blogs/blog.service.ts
@@ -9,7 +9,6 @@ import { BlogBusinessLayer } from './blog.business';
 import { PostQuery } from '../posts/query/post.query';
 import { PaginationResponse } from '../types/pagination-response.type';
 import { deleteIDandV } from '../helpers/simplefy';
-import { validateOrReject } from 'class-validator';
 
 @Injectable()
 export class BlogService {
@@ -62,14 +61,3 @@ export class BlogService {
     return this.blogRepository.deleteOneBlog(id);
   }
 }
-
-const validate0rRejectModel = async (model: any, ctor: { new (): any }) => {
-  if (!(model instanceof ctor)) {
-    throw new Error('Incorrect input data');
-  }
-  try {
-    await validateOrReject(model);
-  } catch (error) {
-    throw new Error(error);
-  }
-};
