884c52084369810e71180fa1653957ae05d41de7
therenatus <therinatus@gmail.com>
2024-01-10 09:36:21 +0600

    app setting

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { BadRequestException, ValidationPipe } from '@nestjs/common';
-import { ErrorExceptionFilter, HttpExceptionFilter } from './exceptions.filter';
+import { appSettings } from './app.settings';
 
-async function bootstrap() {
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      exceptionFactory: (err) => {
-        const errorResponse: any[] = [];
-        err.forEach((error: any) => {
-          const zeroKey = Object.keys(error.constraints)[0];
-          errorResponse.push({
-            message: error.constraints[zeroKey],
-            field: error.property,
-          });
-        });
-        throw new BadRequestException(errorResponse);
-      },
-    }),
-  );
-  app.useGlobalFilters(new ErrorExceptionFilter(), new HttpExceptionFilter());
-  app.setGlobalPrefix('/api');
+  appSettings(app);
   await app.listen(3000);
 }
 bootstrap();
