402b8756e146c736f1d2d249dc49245d153c4983
therenatus <therinatus@gmail.com>
2024-02-25 17:40:22 +0600

    fix: vercel.json 5.0.4

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import express from 'express';
 import { AppModule } from './app.module';
 import { appSettings } from './app.settings';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
+const server = express();
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appSettings(app);
   await app.listen(3000);
+  new ExpressAdapter(server);
 }
 bootstrap();
+export default server;
