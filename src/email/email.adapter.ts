caa21fbdf51c5b9393630428414c876fc57c1a4e
therenatus <therinatus@gmail.com>
2024-02-25 17:00:04 +0600

    fix: email adapter

--- src/email/email.adapter.ts
+++ src/email/email.adapter.ts
@@ -5,8 +5,19 @@ import * as process from 'process';
 @Injectable()
 export class EmailAdapter {
   async sendEmail(email: string, subject: string, message: string) {
+    if (
+      !process.env.MAIL_HOST ||
+      !process.env.MAIL_PORT ||
+      !process.env.MAIL_LOGIN ||
+      !process.env.MAIL_PASS
+    ) {
+      throw new Error(
+        'Missing required environment variables for mail configuration.',
+      );
+    }
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: process.env.MAIL_HOST,
+      port: parseInt(process.env.MAIL_PORT),
       auth: {
         user: process.env.MAIL_LOGIN,
         pass: process.env.MAIL_PASS,
