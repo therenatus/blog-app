7b7447d64b81d53e66b854abda47e61401cd2182
therenatus <therinatus@gmail.com>
2024-02-28 23:06:43 +0600

    add: transform from interface to class and add class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
-export interface CreateUserDto {
+import { IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @MaxLength(10)
+  @MinLength(3)
+  @IsNotEmpty()
   login: string;
+
+  @MaxLength(20)
+  @MinLength(6)
+  @IsNotEmpty()
   password: string;
+
+  @Matches("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")
   email: string;
 }
